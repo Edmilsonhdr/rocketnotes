dd65a964d1c3444b7d66d6850075674c338ea7e2
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-01-09 20:37:39 -0300

    Componente Header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,11 @@
 import { Container } from "./styles.js";
 import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
 export function Details() {
   return (
     <Container>
-      <h1>Qualquer coisa</h1>
-      <p>teste</p>
-      <Button title="a" loading />
-      <Button title="b" />
-      <Button title="c" />
+      <Header />
+      <Button title="Voltar" />
     </Container>
   );
 }
