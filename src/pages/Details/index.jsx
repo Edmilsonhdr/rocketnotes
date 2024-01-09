5f1ad94f5bb15a8d848ab9165693811b4fe8f287
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-01-09 20:25:05 -0300

    Propriedades

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,13 @@
 import { Container } from "./styles.js";
+import { Button } from "../../components/Button";
 export function Details() {
   return (
     <Container>
       <h1>Qualquer coisa</h1>
       <p>teste</p>
+      <Button title="a" loading />
+      <Button title="b" />
+      <Button title="c" />
     </Container>
   );
 }
