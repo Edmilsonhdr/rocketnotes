47dc7f6b89d2aae227a7b427024d090aa7129350
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-01-10 20:40:20 -0300

    section tags

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,11 +2,12 @@ import { Container, Links } from "./styles.js";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 export function Details() {
   return (
     <Container>
       <Header />
-      <Section>
+      <Section title="Links Uteis">
         <Links>
           <li>
             <a href="https://www.youtube.com/"></a>
@@ -15,6 +16,11 @@ export function Details() {
             <a href="https://github.com/"></a>
           </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node" />
       </Section>
       <Button title="Voltar" />
     </Container>
