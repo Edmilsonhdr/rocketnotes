20613041cbb52b47fd081362a1fb87bfa2f07263
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-01-10 20:31:51 -0300

    add section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,21 @@
-import { Container } from "./styles.js";
+import { Container, Links } from "./styles.js";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
 export function Details() {
   return (
     <Container>
       <Header />
+      <Section>
+        <Links>
+          <li>
+            <a href="https://www.youtube.com/"></a>
+          </li>
+          <li>
+            <a href="https://github.com/"></a>
+          </li>
+        </Links>
+      </Section>
       <Button title="Voltar" />
     </Container>
   );
