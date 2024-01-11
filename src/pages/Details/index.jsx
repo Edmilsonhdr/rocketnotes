6291c89163801201667a2af45df3c6d505d671b2
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-01-10 21:02:53 -0300

    justando texto

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./styles.js";
+import { Container, Links, Content } from "./styles.js";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
@@ -8,23 +8,42 @@ export function Details() {
   return (
     <Container>
       <Header />
-      <ButtonText title="Excluir Nota" />
-      <Section title="Links Uteis">
-        <Links>
-          <li>
-            <a href="https://www.youtube.com/"></a>
-          </li>
-          <li>
-            <a href="https://github.com/"></a>
-          </li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota" />
+          <h1>Instrução do React</h1>
+          <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32.
+          </p>
+          <Section title="Links Uteis">
+            <Links>
+              <li>
+                <a href="#">https://www.youtube.com/</a>
+              </li>
+              <li>
+                <a href="#">https://github.com/</a>
+              </li>
+            </Links>
+          </Section>
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="node" />
-      </Section>
-      <Button title="Voltar" />
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
