33bb60db68abf4f16c0b322cea3c51c6a9a5157f
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-01-13 11:26:25 -0300

    Add componente Notas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,8 @@ import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { ButtonText } from "../../components/ButtonText";
+import { Note } from "../../components/Note";
+import { Section } from "../../components/Section";
 
 export function Home() {
   return (
@@ -27,7 +29,19 @@ export function Home() {
         <Input placeholder="Pesquisar titulo" icon={FiSearch} />
       </Search>
 
-      <Content></Content>
+      <Content>
+        <Section title="Minhas Notas">
+          <Note
+            data={{
+              title: "React",
+              tags: [
+                { id: "1", name: "react" },
+                { id: "2", name: "rocketseat" },
+              ],
+            }}
+          />
+        </Section>
+      </Content>
 
       <NewNote>
         <FiPlus />
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -51,7 +51,8 @@ export const Search = styled.div`
 `;
 export const Content = styled.div`
     grid-area: content;
-    background: blue;
+    padding: 0 64px;
+    overflow-y: auto;
 `;
 export const NewNote = styled.button`
     grid-area: newnote;
