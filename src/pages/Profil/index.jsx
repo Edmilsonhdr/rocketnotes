51c675931b38e7e46d2b82995e563217b254856e
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-01-13 21:26:36 -0300

    add avatar usuario

--- src/pages/Profil/index.jsx
+++ src/pages/Profil/index.jsx
@@ -1,7 +1,7 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 export function Profil() {
   return (
     <Container>
@@ -12,6 +12,13 @@ export function Profil() {
       </header>
 
       <Form>
+        <Avatar>
+          <img src="https://github.com/Edmilsonhdr.png" alt="foto usuario" />
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
         <Input placeholder="Nome" type="text" icon={FiUser}></Input>
         <Input placeholder="E-mail" type="text" icon={FiMail}></Input>
         <Input placeholder="Senha Atual" type="password" icon={FiLock}></Input>
