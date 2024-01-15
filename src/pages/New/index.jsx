5f56f7d9ed286c0e4002375182dc0c76c88a6d08
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-01-15 18:29:03 -0300

    header new

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,9 +1,19 @@
 import { Header } from "../../components/Header";
-import { Container } from "./styles";
+import { Input } from "../../components/Input";
+import { Container, Form } from "./styles";
 export function New() {
   return (
     <Container>
       <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Criar Notas</h1>
+            <a href="/">Voltar</a>
+          </header>
+          <Input placeholder="Titulo" />
+        </Form>
+      </main>
     </Container>
   );
 }
