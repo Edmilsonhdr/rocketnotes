74178277da5475ec48ba9b7579e813c82d81c277
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-01-15 19:18:59 -0300

    Estilizando NoteItem

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,8 @@
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { TextArea } from "../../components/TextArea";
+import { NoteItem } from "../../components/NoteItem";
+import { Section } from "../../components/Section";
 import { Container, Form } from "./styles";
 export function New() {
   return (
@@ -14,6 +16,10 @@ export function New() {
           </header>
           <Input placeholder="Titulo" />
           <TextArea placeholder="Observações" />
+          <Section title="Links úteis">
+            <NoteItem value="https://www.google.com/" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
         </Form>
       </main>
     </Container>
