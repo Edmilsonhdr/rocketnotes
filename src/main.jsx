5f0fe429f51ab8559e9b327aa1dc85c08de45468
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-01-15 18:23:11 -0300

    Interface New

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyles from "./styles/global";
 
 import theme from "./styles/theme";
 
-import { Profil } from "./pages/Profil";
+import { New } from "./pages/New";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Profil />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 );
