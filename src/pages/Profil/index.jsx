278cb5330031a3c468e36bd5a53d3d048352355d
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-02-05 20:43:01 -0300

    Carregando dados usuario

--- src/pages/Profil/index.jsx
+++ src/pages/Profil/index.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/auth";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { Container, Form, Avatar } from "./styles";
 export function Profil() {
+  const { user } = useAuth();
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [passwordOld, setPasswordOld] = useState();
+  const [passwordNew, serPasswordNew] = useState();
   return (
     <Container>
       <header>
@@ -20,10 +27,32 @@ export function Profil() {
             <input id="avatar" type="file" />
           </label>
         </Avatar>
-        <Input placeholder="Nome" type="text" icon={FiUser}></Input>
-        <Input placeholder="E-mail" type="text" icon={FiMail}></Input>
-        <Input placeholder="Senha Atual" type="password" icon={FiLock}></Input>
-        <Input placeholder="Nova senha" type="password" icon={FiLock}></Input>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></Input>
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></Input>
+        <Input
+          placeholder="Senha Atual"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPasswordOld(e.target.value)}
+        ></Input>
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => serPasswordNew(e.target.value)}
+        ></Input>
         <Button title="Salvar" />
       </Form>
     </Container>
