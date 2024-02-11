17453deca7eed75a5124fb85dbeecc04f6ffc177
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-02-10 22:54:10 -0300

    remover tags

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -28,6 +28,10 @@ export function New() {
     setNewTag("");
   }
 
+  function handleRemoveTag(deleted) {
+    setTags((prevState) => prevState.filter((tag) => tag !== deleted));
+  }
+
   return (
     <Container>
       <Header />
@@ -58,7 +62,11 @@ export function New() {
           <Section title="Marcadores">
             <div className="tags">
               {tags.map((tag, index) => (
-                <NoteItem key={String(index)} value={tag} onClick={() => {}} />
+                <NoteItem
+                  key={String(index)}
+                  value={tag}
+                  onClick={() => handleRemoveTag(tag)}
+                />
               ))}
               <NoteItem
                 isNew
